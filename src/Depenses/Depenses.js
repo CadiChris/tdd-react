b89b3d5fcb26b27c959f777614bef325cf5d161f
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-01-25 22:59:06 +0100

    Refactor vers useState

--- src/Depenses/Depenses.js
+++ src/Depenses/Depenses.js
@@ -1,79 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import * as PropTypes from "prop-types";
 
-export class Depenses extends Component {
-  state = {
-    payeur: "",
-    montant: "",
-  };
+export function Depenses({ depenses, onDepenser }) {
+  const [payeur, setPayeur] = useState("");
+  const [montant, setMontant] = useState("");
 
-  render() {
-    const { depenses, onDepenser } = this.props;
-    const { payeur, montant } = this.state;
+  return (
+    <div id="depenses" data-testid="depenses">
+      <h3>
+        {depenses.length === 0 && "Aucune dépense"}
+        {depenses.length > 0 &&
+          calculTotalDepense(depenses) + " euros dépensés"}
+      </h3>
 
-    return (
-      <div id="depenses" data-testid="depenses">
-        <h3>
-          {depenses.length === 0 && "Aucune dépense"}
-          {depenses.length > 0 &&
-            this.getTotalDepense() + " euros dépensés"}
-        </h3>
+      <ul>
+        {depenses.map((depense, i) => (
+          <li key={i}>
+            {depense.payeur} : {depense.montant} euros
+          </li>
+        ))}
+      </ul>
 
-        <ul>
-          {depenses.map((depense, i) => (
-            <li key={i}>
-              {depense.payeur} : {depense.montant} euros
-            </li>
-          ))}
-        </ul>
-
-        <div>
-          <label htmlFor="depense-payeur">Payeur</label>
-          <input
-            type="text"
-            id="depense-payeur"
-            value={payeur}
-            onChange={(e) =>
-              this.setState({ payeur: e.target.value })
-            }
-          />
-        </div>
-
-        <div>
-          <label htmlFor="depense-montant">Montant</label>
-          <input
-            type="text"
-            id="depense-montant"
-            value={montant}
-            onChange={(e) =>
-              this.setState({ montant: e.target.value })
-            }
-          />
-        </div>
-
-        <button
-          data-testid="depenser"
-          onClick={() => {
-            onDepenser({
-              payeur,
-              montant: Number(montant),
-            });
-            this.setState({ montant: "", payeur: "" });
-          }}
-        >
-          Dépenser
-        </button>
+      <div>
+        <label htmlFor="depense-payeur">Payeur</label>
+        <input
+          type="text"
+          id="depense-payeur"
+          value={payeur}
+          onChange={(e) => setPayeur(e.target.value)}
+        />
       </div>
-    );
-  }
 
-  getTotalDepense() {
-    const { depenses } = this.props;
-    return depenses.reduce(
-      (total, depense) => total + depense.montant,
-      0
-    );
-  }
+      <div>
+        <label htmlFor="depense-montant">Montant</label>
+        <input
+          type="text"
+          id="depense-montant"
+          value={montant}
+          onChange={(e) => setMontant(e.target.value)}
+        />
+      </div>
+
+      <button
+        data-testid="depenser"
+        onClick={() => {
+          onDepenser({ payeur, montant: Number(montant) });
+          setMontant("");
+          setPayeur("");
+        }}
+      >
+        Dépenser
+      </button>
+    </div>
+  );
+}
+
+function calculTotalDepense(depenses) {
+  return depenses.reduce(
+    (total, depense) => total + depense.montant,
+    0
+  );
 }
 
 Depenses.propTypes = {
