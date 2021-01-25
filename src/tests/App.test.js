e6e841a83ee9b3965c0ac5bc9aee6b45b7cf12fa
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-01-25 22:53:31 +0100

    Supprimer le cleanup appelé automatiquement dans les dernières versions de testing-library

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { cleanup, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 import { cliquerSur, taperAuClavier } from "./aideAuxTests";
-
-afterEach(cleanup);
 
 describe("<App />", () => {
   it("affiche une ardoise vierge", () => {
