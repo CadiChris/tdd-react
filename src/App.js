5039c189c8ca7dde2699458d5b0e71e28b4c2ccb
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-01-25 23:04:00 +0100

    Implémenter l'étape 04

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import "./App.css";
 import { Participants } from "./Participants/Participants";
 import { Depenses } from "./Depenses/Depenses";
+import { calculerLesCreances } from "./Creances/calculerLesCreances";
 
 function App() {
   const [participants, setParticipants] = useState([]);
   const [depenses, setDepenses] = useState([]);
+  const creances = calculerLesCreances(
+    depenses,
+    participants
+  );
 
   return (
     <div className="App">
@@ -24,7 +29,15 @@ function App() {
       />
 
       <div data-testid="creances">
-        <h3>Aucune créance</h3>
+        {creances.length === 0 ? (
+          <h3>Aucune créance</h3>
+        ) : (
+          creances.map((c, i) => (
+            <div key={i}>
+              {`${c.payeur} doit ${c.montant} euros à ${c.receveur}`}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
